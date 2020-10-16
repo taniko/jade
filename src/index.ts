9f2f0526fff247cbb95178d4e2c23c9a473768ae
Yuya Aratani <hrgruri@gmail.com>
2020-10-16 22:32:06 +0900

    add validation

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,16 @@ import {Storage} from "@google-cloud/storage";
 import {KeyManagementServiceClient} from "@google-cloud/kms";
 import {WebClient} from "@slack/web-api";
 import {createReadStream} from 'fs';
+import * as crypto from 'crypto';
 
 exports.jade = async (req: Request, res: Response) => {
+    if (req.method !== 'POST') {
+        res.status(403).send();
+        return;
+    } else if (! await validation(req)) {
+        res.status(403).send();
+        return;
+    }
     const storage = new Storage();
     const bucket = storage.bucket("jade-slack");
     const [files] = await bucket.getFiles({
@@ -20,7 +28,7 @@ exports.jade = async (req: Request, res: Response) => {
     if (name != undefined) {
         const slack: WebClient = new WebClient(await decrypt(process.env.SLACK_TOKEN as string));
         // await slack.files.upload({channels: "", file: createReadStream(`/tmp/${name}`)});
-        console.log(req.body)
+        console.log(JSON.stringify(req.body()));
         res.status(200).send();
     } else {
         res.status(500).send('error');
@@ -43,4 +51,16 @@ const decrypt = async (enc: string) => {
     )
     const [result] = await client.decrypt({name, ciphertext})
     return result.plaintext?.toString().trim()
+}
+
+const validation = async (req: Request): Promise<boolean> => {
+    const timestamp = req.headers["X-Slack-Request-Timestamp"] as string;
+    if (Math.floor(new Date().getTime() / 1000) - parseInt(timestamp, 10) > 60 * 5) {
+        return false;
+    }
+    const signature = req.headers["X-Slack-Signature"] as string;
+    const hmac = crypto.createHmac("sha256", await decrypt(process.env.SIGNING_SECRET as string) as string);
+    const body = req.body();
+    const digest = hmac.update(`v0:${timestamp}:${body}`).digest('hex');
+    return signature === `v0=${digest}`;
 }
