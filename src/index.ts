88408d8b490ced502545105c302ccf67ab05d5f8
Yuya Aratani <hrgruri@gmail.com>
2020-10-17 09:39:47 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,16 @@ import {KeyManagementServiceClient} from "@google-cloud/kms";
 import {WebClient} from "@slack/web-api";
 import {createReadStream} from 'fs';
 import * as crypto from 'crypto';
-import tsscmp from 'tsscmp';
 import qs from 'qs'
 
 exports.jade = async (req: Request, res: Response) => {
-    const slack = new WebClient(await decrypt(process.env.SLACK_TOKEN as string));
-    await slack.chat.postMessage({text: "hello", channel: req.body.channel_id})
     if (req.method !== 'POST') {
-        await slack.chat.postMessage({text: "method", channel: req.body.channel_id})
-        await slack.chat.postMessage({text: req.method, channel: req.body.channel_id})
         res.status(403).send("error");
         return;
     } else if (!await validation(req)) {
-        await slack.chat.postMessage({text: "failed validation", channel: req.body.channel_id})
         res.status(403).send("error");
         return;
     }
-    await slack.chat.postMessage({text: "pass", channel: req.body.channel_id})
     const storage = new Storage();
     const bucket = storage.bucket("jade-slack");
     const [files] = await bucket.getFiles({
@@ -34,12 +27,12 @@ exports.jade = async (req: Request, res: Response) => {
     const name = file.name.split('/').pop()
     await candidate[random(candidate.length)].download({destination: `/tmp/${name}`});
     if (name != undefined) {
-        // await slack.files.upload({channels: req.body.channel_id, file: createReadStream(`/tmp/${name}`)});
-        res.status(200).send("ok");
+        const slack = new WebClient(await decrypt(process.env.SLACK_TOKEN as string));
+        await slack.files.upload({channels: req.body.channel_id, file: createReadStream(`/tmp/${name}`)});
+        return res.status(200).send("ok");
     } else {
-        res.status(500).send('error');
+        return res.status(500).send('error');
     }
-    return;
 }
 
 const random = (max: number) => {
